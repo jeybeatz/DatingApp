afb298c46d14338e04ca2501d42c8fede54e0c43
jeybeatz <podg08@gmail.com>
2019-11-29 19:59:16 -0500

    Section 8-0

--- DatingApp-SPA/src/app/_guards/auth.guard.ts
+++ DatingApp-SPA/src/app/_guards/auth.guard.ts
@@ -5,14 +5,14 @@ import { AlertifyService } from '../_services/alertify.service';
 
 
 @Injectable({
-  providedIn: "root"
+  providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
   constructor(
     private authService: AuthService,
     private router: Router,
     private alertify: AlertifyService
-  ){}
+  ) {}
   canActivate(): boolean {
     if (this.authService.loggedIn()) {
       return true;
